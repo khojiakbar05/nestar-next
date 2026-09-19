464a586892f21c083593d560bf1621db4c03543b
khojiakbar05 <karimjonovkhojiakbar1@gmail.com>
2026-09-19 18:44:15 +0900

    feat: develop TrendProperties component based on react query data

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,14 @@
-import type { AppProps } from 'next/app';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-import { CssBaseline } from '@mui/material';
-import React, { useState } from 'react';
-import { light } from '../scss/MaterialTheme';
-import { ApolloProvider } from '@apollo/client';
-import { useApollo } from '../apollo/client';
 import { appWithTranslation } from 'next-i18next';
-import '../scss/app.scss';
-import '../scss/pc/main.scss';
+import { ApolloProvider } from '@apollo/client';
+import { light } from '../scss/MaterialTheme';
+import { useApollo } from '../apollo/client';
+import { CssBaseline } from '@mui/material';
+import type { AppProps } from 'next/app';
+import React, { useState } from 'react';
 import '../scss/mobile/main.scss';
+import '../scss/pc/main.scss';
+import '../scss/app.scss';
 
 const App = ({ Component, pageProps }: AppProps) => {
 	// @ts-ignore
