8ed6984cf2ca569bad311c75594d3c2d290a1404
khojiakbar05 <karimjonovkhojiakbar1@gmail.com>
2026-09-25 08:06:05 +0900

    feat: develop community related BoardArticles list logic

--- pages/agent/detail.tsx
+++ pages/agent/detail.tsx
@@ -206,7 +206,7 @@ const AgentDetail: NextPage = ({ initialInput, initialComment, ...props }: any) 
 									<div className={'wrap-main'} key={property?._id}>
 										<PropertyBigCard
 											property={property}
-											key={property?._id}
+							 				key={property?._id}
 											likePropertyHandler={likePropertyHandler}
 										/>
 									</div>
